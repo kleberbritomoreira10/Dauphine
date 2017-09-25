5e471704877f36f076ed7499cbd615ccb9d4dffb
MaxOliveira <maxwell_oliveira2@hotmail.com>
2017-09-24 23:50:32 -0300

    Commenting paragraphs and control structures in PStateAttack.cpp, PStateCrouching.cpp and Potion.cpp

--- src/PStateAttack.cpp
+++ src/PStateAttack.cpp
@@ -1,7 +1,16 @@
+/* Dauphine
+* Universidade de Brasília - FGA
+* Técnicas de Programação, 2/2017
+* @PStateAttack.cpp
+* The state when the player is grounded and moving.*/
+
 #include "PStateAttack.h"
 #include "Logger.h"
 #include "Game.h"
 
+/**
+* @see StatePlayer::enter
+*/
 void PStateAttack::enter ()
 {
   this -> box.x = 58;
@@ -14,21 +23,31 @@ void PStateAttack::enter ()
   Game::instance().getAudioHandler().addSoundEffect( "res/audio/FX_NADINE/WOOSH_NADINE_02.wav" );
 }
 
+/**
+* @see StatePlayer::exit
+*/
 void PStateAttack::exit()
 {
   this -> player -> canAttack = true;
 }
 
+/**
+* @see StatePlayer::handleInput
+*/
 void PStateAttack::handleInput ( const std::array<bool, GameKeys::MAX> keyStates_ )
 {
 	( ( void )keyStates_ ); // Unused.
-
+  
   if ( this -> player -> getAnimation() -> getCurrentFrame() == 14 )
   {
     this -> player -> changeState( Player::PStates::IDLE );
   }
 }
 
+/**
+* The constructor.
+* @param player_ : Reference to the player.
+*/
 PStateAttack::PStateAttack( Player* const player_ ) :
   StatePlayer( player_ )
 {
--- src/PStateCrouching.cpp
+++ src/PStateCrouching.cpp
@@ -14,6 +14,7 @@
 void PStateCrouching::enter()
 {
 
+	// Getting bounding box position parameters.
 	this -> box.x = ( int )this -> player -> getWidth() / 4 - 33;
   	this -> box.w = ( int )this -> player -> getWidth() / 1.7;
 	this -> box.y = ( int )this -> player -> getHeight() / 2;
@@ -42,6 +43,7 @@ void PStateCrouching::handleInput( const std::array<bool, GameKeys::MAX> keyStat
 {
 	this -> player -> getAnimation() -> changeAnimation( 8,9,1,false,0 );
 
+	// Getting idle.
 	if ( !keyStates_[GameKeys::CROUCH] )
 	{
 		this -> player -> changeState( Player::PStates::IDLE );
@@ -58,12 +60,14 @@ void PStateCrouching::handleInput( const std::array<bool, GameKeys::MAX> keyStat
 		return;
 	}
 
+	// Moving player.
 	if ( keyStates_[GameKeys::LEFT] || keyStates_[GameKeys::RIGHT] )
 	{
 		this -> player -> changeState( Player::PStates::MOVINGCROUCH );
 		return;
 	}
 
+	// Rolling player.
 	if ( keyStates_[GameKeys::ROLL] )
 	{
 		this -> player -> changeState( Player::PStates::ROLLING );
